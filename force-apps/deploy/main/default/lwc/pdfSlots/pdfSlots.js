3569ce52f3b8be530c6a925e46ec604e772bd33b
@ELTOROIT - Andres Perez <js4Iot@gmail.com>
2024-05-02 18:32:56 -0400

    Change for addressable URL LWC

--- force-apps/deploy/main/default/lwc/pdfSlots/pdfSlots.js
+++ force-apps/deploy/main/default/lwc/pdfSlots/pdfSlots.js
@@ -1,11 +1,20 @@
 import { LightningElement, api } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class PDF_Slots extends LightningElement {
+export default class PDF_Slots extends NavigationMixin(LightningElement) {
 	@api pdfData = {};
 
 	async handlePrintClick() {
-		let pdfData = encodeURIComponent(JSON.stringify(this.pdfData));
-		window.open(`/c/pdfApp.app?pdfData=${pdfData}`, "_blank", "width=700,height=700");
+		// let pdfData = encodeURIComponent(JSON.stringify(this.pdfData));
+		// window.open(`/c/pdfApp.app?pdfData=${pdfData}`, "_blank", "width=700,height=700");
+		this[NavigationMixin.Navigate]({
+			// Pass in pageReference
+			type: "standard__component",
+			attributes: {
+				componentName: "c__pdfPrinter"
+			},
+			state: { ...this.pdfData }
+		});
 	}
 
 	get notPrinting() {
